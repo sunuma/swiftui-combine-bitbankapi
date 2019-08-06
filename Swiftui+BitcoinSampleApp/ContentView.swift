f1389feb528edb6315ebee7c5c34d045a90688d8
ShinUnuma <gagagagiguss@gmail.com>
2019-08-06 16:18:48 +0900

    Initial Commit

---
+++ Swiftui+BitcoinSampleApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Swiftui+BitcoinSampleApp
+//
+//  Created by ShinUnuma on 2019/08/06.
+//  Copyright © 2019 ShinUnuma. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
